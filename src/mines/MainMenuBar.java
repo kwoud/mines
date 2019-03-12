3d9d3bbc8e031cc34f32c99658d44f7bd1723a1c
Kieran Austin <kieranaustin@gmail.com>
2019-03-12 12:46:48 +0100

    created basic menu bar with dummy menus and menu items

---
+++ src/mines/MainMenuBar.java
@@ -0,0 +1,33 @@
+package mines;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class MainMenuBar extends JMenuBar {
+	public MainMenuBar() {
+		// Create and add menus to menu bar
+		JMenu fileMenu = new JMenu("File");
+		JMenu optionsMenu = new JMenu("Options");
+		JMenu helpMenu = new JMenu("Help");
+		this.add(fileMenu);
+		this.add(optionsMenu);
+		this.add(helpMenu);
+		
+		// create and add menu items to file menu
+		JMenuItem fileItemOpen = new JMenuItem("Open...");
+		JMenuItem fileItemSave = new JMenuItem("Save...");
+		JMenuItem fileItemExit = new JMenuItem("Exit");
+		fileMenu.add(fileItemOpen);
+		fileMenu.add(fileItemSave);
+		fileMenu.add(fileItemExit);
+		
+		// create and add menu items to options menu
+		JMenuItem optionsItemSetSize = new JMenuItem("Set grid size...");
+		optionsMenu.add(optionsItemSetSize);
+		
+		// create and add menu items to help menu
+		JMenuItem helpItemAbout = new JMenuItem("About");
+		helpMenu.add(helpItemAbout);
+	}
+}
--- src/mines/StartWindow.java
+++ src/mines/StartWindow.java
@@ -1,6 +1,11 @@
 package mines;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.SwingUtilities;
 
 public class StartWindow {
@@ -10,7 +15,15 @@ public class StartWindow {
 		frame.setSize(20*60, 15*60);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// set menu bar
+		JMenuBar menuBar = new MainMenuBar();
+		menuBar.setPreferredSize(new Dimension(100, 25));
+		frame.setJMenuBar(menuBar);
+		
+		// show frame
 		frame.setVisible(true);
+		//JOptionPane.showMessageDialog(frame, "Welcome to kwoudMines!");
 	}
 	
 	public static void main(String[] args) {
