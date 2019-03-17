5b51a063b604e091c4092ea30ac886b052fa8ef5
Kieran Austin <kieranaustin@gmail.com>
2019-03-17 12:59:42 +0100

    rewritten, such as Grid functions as backend

--- src/mines/Grid.java
+++ src/mines/Grid.java
@@ -3,13 +3,12 @@ package mines;
 import java.util.Random;
 
 public class Grid {
-
 	private int[] gridArray;
-	int gridX = StartWindow.getGridX();
-	int gridY = StartWindow.getGridY();
-	int gridSize = gridX * gridY;
-	int numMines = StartWindow.getNumMines();
-	
+	private int gridX = 15;
+	private int gridY = 10;
+	private int gridSize = gridX * gridY;
+	private int numMines = 30;
+
 	public Grid() {
 		gridArray = new int[gridSize];
 	}
@@ -17,15 +16,42 @@ public class Grid {
 	public int[] getGrid() {
 		return gridArray;
 	}
-	
+
 	public int getValue(int pos) {
 		return gridArray[pos];
 	}
-	
+
 	public String toString(int pos) {
-		if (gridArray[pos] == 9) { return "*"; }
-		if (gridArray[pos] == 0) { return " "; }
+		if (gridArray[pos] == 9) {return "*";}
+		if (gridArray[pos] == 0) {return " ";}
 		return Integer.toString(gridArray[pos]);
+	}
+
+	public int getGridX() {return gridX;}
+	public int getGridY() {return gridY;}
+	public int getGridSize() {return gridSize;}
+	public int getNumMines() {return numMines;}
+	
+	public void setGridX(int x) {
+		gridX = x;
+		gridSize = gridX * gridY;
+	}
+
+	public void setGridY(int y) {
+		gridY = y;
+		gridSize = gridX * gridY;
+	}
+	
+	public void setGridSize(int x, int y) {
+		gridX = x;
+		gridY = y;
+		gridSize = gridX * gridY;
+	}
+	
+	public void setNumMines(int m) {
+		assert(m <= gridX * gridY);
+		assert(m > 0);
+		numMines = m;
 	}
 	
 	public void setGridMines() {
@@ -36,48 +62,45 @@ public class Grid {
 			if (gridArray[randPos] == 0) {
 				gridArray[randPos] = 9; // 9 steht für eine Mine
 				mineCount--;
-			}	
-		}
-	}
-	
-	private boolean inGrid(int i, int j) {
-		if ((i < 0) || (i >= gridX) ||
-				(j < 0) || (j >= gridY)) {
-			return false;
 			}
-		else return true;
-	}
-	
-	private boolean isMine(int i, int j) {
-		if (gridArray[(j * gridX) + i] == 9) {
-			return true;
 		}
-		else return false;
 	}
-	
-	public int[][] psblNeighbour(int i, int j) {
-		int[][] neighbourArray = new int[][] {	{i - 1, j - 1}, {i, j - 1}, {i + 1, j - 1}, 
-												{i - 1 , j}, 				{i + 1, j},
-												{i - 1, j + 1}, {i, j + 1}, {i + 1, j + 1}	}; 
+
+	private boolean inGrid(int x, int y) {
+		if ((x < 0) || (x >= gridX) || (y < 0) || (y >= gridY)) {
+			return false;
+		} else
+			return true;
+	}
+
+	private boolean isMine(int x, int y) {
+		if (gridArray[x + (y * gridX)] == 9) {
+			return true;
+		} else
+			return false;
+	}
+
+	public static int[][] getNeighbour(int x, int y) {
+		int[][] neighbourArray = new int[][] { { x - 1, y - 1 }, { x, y - 1 }, { x + 1, y - 1 }, { x - 1, y },
+				{ x + 1, y }, { x - 1, y + 1 }, { x, y + 1 }, { x + 1, y + 1 } };
 		return neighbourArray;
 	}
-	
+
 	public void setGridNumbers() {
+		int currX, currY;
 		for (int j = 0; j < gridY; j++) {
 			for (int i = 0; i < gridX; i++) {
-				if (gridArray[(j * gridX) + i] == 0) {
+				if (gridArray[i + (j * gridX)] == 0) {
 					for (int neighbour = 0; neighbour < 8; neighbour++) {
-						int currX = psblNeighbour(i, j)[neighbour][0];
-						int currY = psblNeighbour(i, j)[neighbour][1];
-						if (inGrid(currX, currY)) {
-							if (isMine(currX, currY)) {
-								gridArray[(j * gridX) + i]++;							
-							}
+						currX = getNeighbour(i, j)[neighbour][0];
+						currY = getNeighbour(i, j)[neighbour][1];
+						if ((inGrid(currX, currY)) && (isMine(currX, currY))) {
+							gridArray[i + (j * gridX)]++;
 						}
 					}
 				}
 			}
 		}
 	}
-	
+
 }
--- src/mines/StartWindow.java
+++ src/mines/StartWindow.java
@@ -7,11 +7,9 @@ import javax.swing.SwingUtilities;
 
 
 public class StartWindow {
+	private static Grid grid;
 	private static MainFrame frame;
 	// set private class variables to standard values
-	private static int gridX = 15;
-	private static int gridY = 10;
-	private static int numMines = 30;
 	private static int buttonSize = 24;
 	private static int incrSize = 5;
 	
@@ -20,8 +18,9 @@ public class StartWindow {
 	
 	private static void createAndShowGUI()  {		
 		// initiate main frame and create buttons through <MainFrame> constructor
-		frame = new MainFrame("kwoudMines", gridX, gridY);
-		frame.setSize(gridX*buttonSize, gridY*buttonSize+_MENUSIZE_);
+		grid = new Grid();
+		frame = new MainFrame("kwoudMines", grid.getGridX(), grid.getGridY());
+		setFrameSize();
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -36,36 +35,39 @@ public class StartWindow {
 	
 	public static JFrame getFrame() {return frame;}
 	 
-	public static int getGridX() {return gridX;}
+	public static int getGridX() {return grid.getGridX();}
 	
-	public static int getGridY() {return gridY;}
+	public static int getGridY() {return grid.getGridY();}
 	
-	public static void setNumMines(int num) {numMines=num;}
+	public static void setNumMines(int num) {grid.setNumMines(num);}
 	
-	public static int getNumMines() {return numMines;}
+	public static int getNumMines() {return grid.getNumMines();}
 	
 	public static int getButtonSize() {return buttonSize;}
 	
+	public static void setFrameSize() {
+		frame.setSize(grid.getGridX()*buttonSize, grid.getGridY()*buttonSize+_MENUSIZE_); // find better way to deal with menu size; buttons are now squares; maybe introduce new method setSize for class
+	}
+	
 	public static void increaseSize() {
 		buttonSize += incrSize;
-		frame.setSize(gridX*buttonSize, gridY*buttonSize+_MENUSIZE_);
+		setFrameSize();
 		frame.revalidate();
 	}
 	
 	public static void decreaseSize() {
 		buttonSize -= incrSize;
-		frame.setSize(gridX*buttonSize, gridY*buttonSize+_MENUSIZE_);
+		setFrameSize();
 		frame.revalidate();
 	}
 	
 	public static void setGridSize(int x, int y) {
-		gridX = x;
-		gridY = y;
+		grid.setGridSize(x, y);
 		frame.removeAll();
-		frame.setGridSize(gridX, gridY);
+		frame.setGridSize(grid.getGridX(), grid.getGridY());
 		frame.createButtons();
 		frame.addButtons();
-		frame.setSize(gridX*buttonSize, gridY*buttonSize+_MENUSIZE_); // find better way to deal with menu size; buttons are now squares; maybe introduce new method setSize for class
+		setFrameSize();
 		frame.revalidate();
 		frame.repaint();
 	}
